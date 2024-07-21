16bf59f2d92cfb4f2f975d98ce455738d5c3005a
Evgeniy Ganeev <mamkin.itshnik@gmail.com>
2024-07-21 13:45:59 +0300

    init

---
+++ lixo-ios/lixo-ios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  lixo-ios
+//
+//  Created by user on 21.07.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ lixo-ios/lixo-ios/lixo_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  lixo_iosApp.swift
+//  lixo-ios
+//
+//  Created by user on 21.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct lixo_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
